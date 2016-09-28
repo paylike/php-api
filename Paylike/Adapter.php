00c9c99dc04c1e849b8e601d1a1a342b0da86f01
Ionut Calara <ionut.calara@gmail.com>
2016-09-28 18:13:46 +0300

    Refactored, added Readme

--- Paylike/Adapter.php
+++ Paylike/Adapter.php
@@ -1,6 +1,5 @@
 <?php
 namespace Paylike;
-
 /**
  * Class Adapter
  * @package Paylike
@@ -13,59 +12,56 @@ namespace Paylike;
  */
 class Adapter {
 
-    public $apiUrl = 'https://api.paylike.io';
-    private $apiKey;
+	public $api_url = 'https://api.paylike.io';
+	private $api_key;
 
-    /**
-     * Adapter constructor.
-     *
-     * @param $privateApiKey
-     */
-    public function __construct( $privateApiKey ) {
-        if ( $privateApiKey ) {
-            $this->setApiKey( $privateApiKey );
-        } else {
-            trigger_error('Private Key is missing!',E_USER_ERROR);
-            return null;
-        }
-    }
+	/**
+	 * Adapter constructor.
+	 *
+	 * @param $private_api_key
+	 */
+	public function __construct( $private_api_key ) {
+		$this->setApiKey( $private_api_key );
+	}
 
-    /**
-     * @param $key
-     * set the api key.
-     */
-    public function setApiKey( $key ) {
-        $this->apiKey = $key;
-    }
 
-    /**
-     * @param $url this is required, do not use the full url,
-     * only prepend the params eg: transactions/' . $transactionId . '/captures'
-     * @param $data this is optional
-     * Actual call to the api via curl.
-     *
-     * @return bool|mixed
-     */
-    public function request( $url, $data = null ) {
-        $url = $this->apiUrl . '/' . $url;
-        $ch  = curl_init();
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        curl_setopt( $ch, CURLOPT_HEADER, false );
-        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-        curl_setopt( $ch, CURLOPT_USERPWD, ":" . $this->apiKey );
-        if ( $data ) {
-            curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
-        }
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-        $result   = curl_exec( $ch );
-        $httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
-        curl_close( $ch );
-        $output = json_decode( $result, true );
-        if ( $httpCode >= 200 || $httpCode <= 299 ) {
-            return $output;
-        } else {
-            return false;
-        }
-    }
+	/**
+	 * @param $key
+	 * set the api key.
+	 */
+	public function setApiKey( $key ) {
+		$this->api_key = $key;
+	}
+
+	/**
+	 * @param $url this is required, do not use the full url,
+	 * only prepend the params eg: transactions/' . $transaction_id . '/captures'
+	 * @param $data this is optional
+	 * Actual call to the api via curl.
+	 *
+	 * @return bool|mixed
+	 */
+	public function request( $url, $data = null ) {
+		$url = $this->api_url . '/' . $url;
+		$ch  = curl_init();
+		curl_setopt( $ch, CURLOPT_URL, $url );
+		curl_setopt( $ch, CURLOPT_HEADER, false );
+		curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+		curl_setopt( $ch, CURLOPT_USERPWD, ":" . $this->api_key );
+		if ( $data ) {
+			curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
+		}
+		curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+		$result   = curl_exec( $ch );
+		$httpcode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+		curl_close( $ch );
+		$output = json_decode( $result, true );
+		if ( $httpcode >= 200 || $httpcode <= 299 ) {
+			return $output;
+		} else {
+			return false;
+		}
+	}
+
 
 }
--- examples.php
+++ examples.php
@@ -1,220 +1,210 @@
 <?php
 session_start();
 include_once( 'Paylike/Client.php' );
-$privateAppKey = 'c0d3d84c-4ced-4f07-963f-f3e5b6196e75';
-$publicKey     = '6bb16eb9-de0f-4224-9bc1-3d2b35b0bccd';
-$amount        = 5000; //cents;
-$currency      = 'GBP';
+$private_app_key = 'c0d3d84c-4ced-4f07-963f-f3e5b6196e75';
+$public_key      = '6bb16eb9-de0f-4224-9bc1-3d2b35b0bccd';
+$amount          = 5000; //cents;
+$currency        = 'GBP';
 if ( isset( $_POST['action'] ) ) {
-    \Paylike\Client::setKey( $privateAppKey );
-    $transactionId             = $_POST['transactionId'];
-    $_SESSION['transactionId'] = $transactionId;
-    switch ( $_POST['action'] ) {
-        case "voidTransactionFull":
-            $response = \Paylike\Transaction::void( $transactionId, array( 'amount' => $amount ) );
-            break;
-        case "voidTransactionHalf":
-            $response = \Paylike\Transaction::void( $transactionId, array( 'amount' => $amount / 2 ) );
-            break;
-        case "fetchTransaction":
-            $response = \Paylike\Transaction::fetch( $transactionId );
-            break;
-        case "captureTransactionFull":
-            $data     = array(
-                'amount'   => $amount / 2,
-                'currency' => $currency
-            );
-            $response = \Paylike\Transaction::capture( $transactionId, $data );
-            break;
-        case "captureTransactionHalf":
-            $data     = array(
-                'amount'   => $amount / 2,
-                'currency' => $currency
-            );
-            $response = \Paylike\Transaction::capture( $transactionId, $data );
-            break;
-        case "refundTransactionFull":
-            $data     = array(
-                'amount'     => $amount,
-                'descriptor' => $_POST['reason']
-            );
-            $response = \Paylike\Transaction::refund( $transactionId, $data );
-            break;
-        case "refundTransactionHalf":
-            $data     = array(
-                'amount'     => $amount / 2,
-                'descriptor' => $_POST['reason']
-            );
-            $response = \Paylike\Transaction::refund( $transactionId, $data );
-            break;
-    }
+	\Paylike\Client::setKey( $private_app_key );
+	$transaction_id             = $_POST['transaction_id'];
+	$_SESSION['transaction_id'] = $transaction_id;
+	switch ( $_POST['action'] ) {
+		case "void_transaction_full":
+			$response = \Paylike\Transaction::void( $transaction_id, $amount );
+			break;
+		case "void_transaction_half":
+			$response = \Paylike\Transaction::void( $transaction_id, $amount / 2 );
+			break;
+		case "check_transaction_authorization":
+			$response = \Paylike\Transaction::authorize( $transaction_id );
+			break;
+		case "capture_transaction_full":
+			$response = \Paylike\Transaction::capture( $transaction_id, $amount, $currency );
+			break;
+		case "capture_transaction_half":
+			$response = \Paylike\Transaction::capture( $transaction_id, $amount / 2, $currency );
+			break;
+		case "refund_transaction_full":
+			$response = \Paylike\Transaction::refund( $transaction_id, $amount, $_POST['reason'] );
+			break;
+		case "refund_transaction_half":
+			$response = \Paylike\Transaction::refund( $transaction_id, $amount / 2, $_POST['reason'] );
+			break;
+	}
 }
-if ( ! isset( $transactionId ) ) {
-    if ( isset( $_SESSION['transactionId'] ) ) {
-        $transactionId = $_SESSION['transactionId'];
-    }
+if ( ! isset( $transaction_id ) ) {
+	if ( isset( $_SESSION['transaction_id'] ) ) {
+		$transaction_id = $_SESSION['transaction_id'];
+	}
 }
 ?>
 <html>
 <head>
-    <title>Paylike test</title>
-    <link type="text/css" href="css/normalize.css"/>
-    <link type="text/css" href="css/main.css"/>
+	<title>Paylike test</title>
+	<link type="text/css" href="css/normalize.css"/>
+	<link type="text/css" href="css/main.css"/>
 </head>
 <body>
 <h2>
-    Test Paylike Api Wrapper
+	Test Paylike Api Wrapper
 </h2>
 <br/>
 <?php
 if ( isset( $response ) ) {
-    echo '<div id="result"><pre>';
-    print_r( $response );
-    echo '</pre>';
-    if ( isset( $response['transaction']['successful'] ) && $response['transaction']['successful'] ) {
-        echo '<div style="color:rgb(69, 110, 16)">Operation was successful.</div>';
-    } else {
-        echo '<div style="color:#fe171d">Operation failed.</div>';
-    }
-    echo '</div>';
+	echo '<div id="result"><pre>';
+	print_r( $response );
+	echo '</pre>';
+	if ( isset( $response['transaction']['successful'] ) && $response['transaction']['successful'] ) {
+		echo '<div style="color:rgb(69, 110, 16)">Operation was successful.</div>';
+	} else {
+		echo '<div style="color:#fe171d">Operation failed.</div>';
+	}
+	echo '</div>';
 }
 ?>
 <button onclick="pay();">Get transaction id</button>
 <script src="https://sdk.paylike.io/3.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
 <script>
-    var paylike = Paylike('<?php echo $publicKey;?>');
+	var paylike = Paylike('<?php echo $public_key ;?>');
 
-    function pay() {
+	function pay() {
 
-        paylike.popup({
-            title: 'Testing the payment',
-            currency: '<?php echo $currency; ?>',
-            amount: <?php echo $amount; ?>,
-            custom: {
-                orderNo: 'Test Order',
-                email: 'ionut@derikon.com',
-            },
-        }, function (err, res) {
-            if (err)
-                return console.warn(err);
-            var trxid = res.transaction.id;
-            jQuery(".transactionId").val(trxid);
-        });
-    }
+		paylike.popup({
+			title: 'Testing the payment',
+			currency: '<?php echo $currency; ?>',
+			amount: <?php echo $amount; ?>,
+			custom: {
+				orderNo: 'Test Order',
+				email: 'ionut@derikon.com',
+			},
+		}, function (err, res) {
+			if (err)
+				return console.warn(err);
+			var trxid = res.transaction.id;
+			jQuery(".transaction_id").val(trxid);
+		});
+	}
 </script>
 <br/>
+
 <form method="POST">
-    <h4>
-        Void the transaction for the entire amount
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId">
-    <input type="hidden" name="action" value="voidTransactionFull">
-    <input type="submit" value="Test Void"/>
+	<h4>
+		Void the transaction for the entire amount
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id">
+	<input type="hidden" name="action" value="void_transaction_full">
+	<input type="submit" value="Test Void"/>
 </form>
 <br/>
+
 <form method="POST">
-    <h4>
-        Void the transaction for half of amount
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId">
-    <input type="hidden" name="action" value="voidTransactionHalf">
-    <input type="submit" value="Test Void"/>
+	<h4>
+		Void the transaction for half of amount
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id">
+	<input type="hidden" name="action" value="void_transaction_half">
+	<input type="submit" value="Test Void"/>
 </form>
 <br/>
+
 <form method="POST">
-    <h4>
-        Fetch transaction.
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId">
-    <input type="hidden" name="action" value="fetchTransaction">
-    <input type="submit" value="Test Transaction fetch"/>
+	<h4>
+		Check authorization
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id">
+	<input type="hidden" name="action" value="check_transaction_authorization">
+	<input type="submit" value="Test Authorization"/>
 </form>
 <br/>
+
 <form method="POST">
-    <h4>
-        Capture the transaction for the full amount
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId">
-    <input type="hidden" name="action" value="captureTransactionFull">
-    <input type="submit" value="Test Capture"/>
+	<h4>
+		Capture the transaction for the full amount
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id">
+	<input type="hidden" name="action" value="capture_transaction_full">
+	<input type="submit" value="Test Capture"/>
 </form>
 <br/>
+
 <form method="POST">
-    <h4>
-        Capture the transaction for the half of the amount
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId">
-    <input type="hidden" name="action" value="captureTransactionHalf">
-    <input type="submit" value="Test Capture"/>
+	<h4>
+		Capture the transaction for the half of the amount
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id">
+	<input type="hidden" name="action" value="capture_transaction_half">
+	<input type="submit" value="Test Capture"/>
 </form>
 <form method="POST">
-    <h4>
-        Refund the transaction for the full amount
-        <br/>
-        <small><i>
-                Only works when you have previously captured the amount.
-            </i></small>
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId"><br/>
-    <label>
-        Reason for the refund(optional)
-    </label>
-    <input type="text" name="reason">
-    <input type="hidden" name="action" value="refundTransactionFull">
-    <input type="submit" value="Test Refund"/>
+	<h4>
+		Refund the transaction for the full amount
+		<br/>
+		<small><i>
+				Only works when you have previously captured the amount.
+			</i></small>
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id"><br/>
+	<label>
+		Reason for the refund(optional)
+	</label>
+	<input type="text" name="reason">
+	<input type="hidden" name="action" value="refund_transaction_full">
+	<input type="submit" value="Test Refund"/>
 </form>
 <br/>
+
 <form method="POST">
-    <h4>
-        Capture the transaction for the half of the amount
-        <br/>
-        <small><i>
-                Only works when you have previously captured the amount.
-            </i></small>
-    </h4>
-    <label>
-        Transaction id
-    </label>
-    <input type="text" name="transactionId" <?php if ( isset( $transactionId ) ) {
-        echo 'value="' . $transactionId . '"';
-    } ?> class="transactionId"><br/>
-    <label>
-        Reason for the refund(optional)
-    </label>
-    <input type="text" name="reason">
-    <input type="hidden" name="action" value="refundTransactionHalf">
-    <input type="submit" value="Test Refund"/>
+	<h4>
+		Capture the transaction for the half of the amount
+		<br/>
+		<small><i>
+				Only works when you have previously captured the amount.
+			</i></small>
+	</h4>
+	<label>
+		Transaction id
+	</label>
+	<input type="text" name="transaction_id" <?php if ( isset( $transaction_id ) ) {
+		echo 'value="' . $transaction_id . '"';
+	} ?> class="transaction_id"><br/>
+	<label>
+		Reason for the refund(optional)
+	</label>
+	<input type="text" name="reason">
+	<input type="hidden" name="action" value="refund_transaction_half">
+	<input type="submit" value="Test Refund"/>
 </form>
 </body>
 </html>
